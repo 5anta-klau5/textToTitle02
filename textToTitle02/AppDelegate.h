943e14670d986b298e4acc7d56f330bda69c2a61
Sergey Sergienko <santa-klaus88@bk.ru>
2016-07-16 19:32:41 +0300

    Initial Commit

---
+++ textToTitle02/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textToTitle02
+//
+//  Created by Serhii Serhiienko on 7/16/16.
+//  Copyright © 2016 Serhii Serhiienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ textToTitle02/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  textToTitle02
+//
+//  Created by Serhii Serhiienko on 7/16/16.
+//  Copyright © 2016 Serhii Serhiienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
